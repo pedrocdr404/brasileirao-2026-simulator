b79020a935dbbec3f1a49cc954eb1bd8c72b7873
Pedro R. <pedro.moreira@discente.ufj.edu.br>
2026-08-19 16:10:07 -0300

    fix app.jsx browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Serie from './pages/Serie'
 import Admin from './pages/Admin'
- 
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/"        element={<Home />} />
-        <Route path="/serie-a" element={<Serie serie="A" />} />
-        <Route path="/admin"   element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/"        element={<Home />} />
+      <Route path="/serie-a" element={<Serie />} />
+      <Route path="/admin"   element={<Admin />} />
+    </Routes>
   )
 }
- 
